5c299b787291e3f5fed21856fcb647cbeadf8da9
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2026-05-22 20:53:20 +0530

    OTP with Redis

---
+++ 04 OTP Login with TTL/src/index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import Redis from 'ioredis';
+
+const app = express();
+app.use(express.json());
+
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+
+function otpKey(phone) {
+  return `otp:${phone}`;
+}
+
+app.post('/otp', async (req, res) => {
+    const { phone } = req.body;
+
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    await redis.set(otpKey(phone), otp, 'EX', 30);
+
+    res.json({ message: 'OTP generated successfully', phone, otp });
+});
+
+app.post('/otp/verify', async (req, res) => {
+    const { phone, otp } = req.body;
+
+    const storedOtp = await redis.get(otpKey(phone));
+
+    if(!storedOtp) {
+        return res.status(400).json({ message: 'OTP expired or not found' });
+    }
+
+    if (storedOtp === otp) {
+        await redis.del(otpKey(phone));
+        res.json({ message: 'OTP verified successfully' });
+    }
+    else {
+        res.status(400).json({ message: 'Invalid OTP' });
+    }
+
+    await redis.del(otpKey(phone));
+});
+
+app.get('/otp/:phone/ttl', async (req, res) => {
+    const ttl = await redis.ttl(otpKey(req.params.phone));
+
+    res.json({ ttl });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
